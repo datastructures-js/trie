490fc0349f2076f61bebf8d9fb0abd4af104a7ec
Eyas Ranjous <eyas.ranjous@gmail.com>
2020-03-23 21:10:39 -0600

    TrieNode

---
+++ src/trieNode.js
@@ -0,0 +1,94 @@
+/**
+ * datastructures-js/trie
+ * @copyright 2020 Eyas Ranjous <eyas.ranjous@gmail.com>
+ * @license MIT
+ */
+
+/**
+ * @class TrieNode
+ */
+class TrieNode {
+  constructor(char) {
+    this.char = char;
+    this.endOfWord = false;
+    this.parentNode = null;
+    this.children = new Map();
+  }
+
+  /**
+   * @returns {string}
+   */
+  getChar() {
+    return this.char;
+  }
+
+  /**
+   * @param {TrieNode} parentNode
+   */
+  setParent(parentNode) {
+    this.parentNode = parentNode;
+  }
+
+  /**
+   * @return {TrieNode}
+   */
+  getParent() {
+    return this.parentNode;
+  }
+
+  /**
+   * @param {boolean} endOfWord
+   */
+  setEndOfWord(endOfWord) {
+    this.endOfWord = endOfWord;
+  }
+
+  /**
+   * @return {boolean}
+   */
+  isEndOfWord() {
+    return this.endOfWord;
+  }
+
+  /**
+   * @param {string} char
+   */
+  addChild(char) {
+    const childNode = new TrieNode(char);
+    childNode.setParent(this);
+    this.children.set(char, childNode);
+  }
+
+  /**
+   * @param {string} char
+   * @return {boolean}
+   */
+  removeChild(char) {
+    return this.children.delete(char);
+  }
+
+  /**
+   * @param {string} char
+   * @return {TrieNode}
+   */
+  getChild(char) {
+    return this.children.get(char) || null;
+  }
+
+  /**
+   * @param {string} char
+   * @return {boolean}
+   */
+  hasChild(char) {
+    return this.childen.has(char);
+  }
+
+  /**
+   * @return {Map}
+   */
+  getChildren() {
+    return this.children;
+  }
+}
+
+module.exports = TrieNode;
