a791aeba43cfe63c866d1867ae1c97cc06437578
Eyas Ranjous <eyas.ranjous@gmail.com>
2021-06-20 10:32:54 +0200

    toString

--- src/trie.d.ts
+++ src/trie.d.ts
@@ -1,10 +1,10 @@
 import { TrieNode } from './trieNode';
 
 export class Trie {
-  insert(value: string): Trie;
-  has(value: string): boolean;
-  find(value: string): TrieNode;
-  remove(value: string): string|null;
+  insert(value: { toString: () => string }): Trie;
+  has(value: { toString: () => string }): boolean;
+  find(value: { toString: () => string }): TrieNode;
+  remove(value: { toString: () => string }): string|null;
   forEach(cb: (word: string) => void): void;
   toArray(): string[];
   nodesCount(): number;
